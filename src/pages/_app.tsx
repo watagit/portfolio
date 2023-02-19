e585541110dcf29578985dba281f3a5ce58ff37c
watagit <wtr0414@gmail.com>
2023-02-19 14:18:17 +0900

    feat: set ogp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import type { AppProps } from "next/app";
+import NextHeadSeo from "next-head-seo";
 import "tailwindcss/tailwind.css";
+
 import { Header } from "@/component/ui/Header";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <NextHeadSeo
+        title="Wataru Ono"
+        description="Wataru Ono's portfolio page"
+        og={{
+          title: "Wataru Ono",
+          image: "/og.png",
+        }}
+      />
       <Header />
       <main className="mx-auto w-11/12 sm:w-2/3">
         <Component {...pageProps} />
