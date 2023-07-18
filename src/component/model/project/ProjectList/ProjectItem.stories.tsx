4a4987e0b89de175a804ad5d1bb74fbec803c48f
watagit <wtr0414@gmail.com>
2023-07-18 16:50:23 +0900

    feat: create story for project model component

---
+++ src/component/model/project/ProjectList/ProjectItem.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ProjectItem } from "./ProjectItem";
+import { projects } from "@/data/project";
+
+const meta: Meta<typeof ProjectItem> = {
+  component: ProjectItem,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProjectItem>;
+
+export const Default: Story = {
+  args: {
+    project: projects[0],
+  },
+};
