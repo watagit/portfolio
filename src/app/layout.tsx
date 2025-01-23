faa86a21cdcc36177bb8ebc573dfe771e002ff00
Wataru Ono <wataru.ono@dena.com>
2025-01-23 21:26:48 +0900

    ファイルベースのfavicon設定に変更する

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { type Metadata } from "next";
-import Head from "next/head";
 import type { FC, ReactNode } from "react";
 
 import { Header } from "~/component/ui/Header";
@@ -23,33 +22,6 @@ type RootLayoutProps = {
 const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <body>
         <Header />
         <main className="mx-auto w-11/12 sm:w-2/3">{children}</main>
