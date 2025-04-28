85648da3bdc884c70259966ac98a6fe794ab066e
Wataru Ono <wataru.ono@dena.com>
2025-04-28 17:43:49 +0900

    eslintを使う

--- next.config.js
+++ next.config.js
@@ -4,15 +4,33 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-      },
-    ],
-  },
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "raw.githubusercontent.com",
+			},
+		],
+	},
+
+	async headers() {
+		return [
+			{
+				source: "/",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*", // Set your origin
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = withVanillaExtract(nextConfig);
