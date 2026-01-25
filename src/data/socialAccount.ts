78c0decba65500dc01d005438c06f19bfd61db70
watagit <wtr0414@gmail.com>
2026-01-25 12:05:43 +0900

    アイコンサイズを調整する

--- src/data/socialAccount.ts
+++ src/data/socialAccount.ts
@@ -1,6 +1,6 @@
 import { type SocialAccount } from "~/domain/socialAccount";
 
 export const socialAccounts: Array<SocialAccount> = [
-  { id: "1", accountType: "x", url: "https://twitter.com/what_a_pon" },
-  { id: "2", accountType: "github", url: "https://github.com/watagit" },
+  { id: "1", accountType: "x", url: "https://twitter.com/what_a_pon", size: 22 },
+  { id: "2", accountType: "github", url: "https://github.com/watagit", size: 24 },
 ];
